a3a0315fa2f3614911ea03cc8d5d89df71d320cf
Yogita Patil <yogitapatil8571@gmail.com>
2026-08-11 22:24:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Inverted Right Angle Triangle Pattern/inverted-right-angle-triangle-pattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+
+        sc.close();
+    }
+}
