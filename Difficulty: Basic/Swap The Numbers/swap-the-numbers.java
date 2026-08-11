5167d5560a2e0704692cd90cce0439b9fb68950e
Yogita Patil <yogitapatil8571@gmail.com>
2026-08-11 22:32:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void swap(int a, int b) {
+        // code here
+        int temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println(a + " " + b);
+    }
+}
