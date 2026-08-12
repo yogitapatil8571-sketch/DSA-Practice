7e3296270e980a78b80333e584488d568f608a56
Yogita Patil <yogitapatil8571@gmail.com>
2026-08-12 22:27:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Even Positioned Characters/even-positioned-characters.java
@@ -0,0 +1,8 @@
+class Solution {
+    public static void utility(String s) {
+        // code here
+        for(int i=0;i<s.length();i+=2){
+            System.out.print(s.charAt(i));
+        }
+    }
+}
