e3bb242124a31cef2b1229dab3387450d73bfbb5
Yogita Patil <yogitapatil8571@gmail.com>
2026-08-12 22:41:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The If Statement/the-if-statement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // code here
+        if(n>100){
+            System.out.print("Big\n");
+        }
+        
+            System.out.print("Number");
+
+
+    }
+}
