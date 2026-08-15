747513d0035dd7da4cc48331d2dc1bc8f3137d23
Yogita Patil <yogitapatil8571@gmail.com>
2026-08-15 21:41:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Vowel or Not/vowel-or-not.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        // code here
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'|| ch=='A' || ch=='E' || ch=='I' || ch=='O' ||ch=='U'){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
