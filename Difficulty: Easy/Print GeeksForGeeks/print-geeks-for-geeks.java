043468a2472587a31bd6483b0b3b15feb2a6a327
Yogita Patil <yogitapatil8571@gmail.com>
2026-08-18 22:21:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print GeeksForGeeks/print-geeks-for-geeks.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Prints Hello World
+        // System.out.println("Hello World");
+        // Prints GeeksForGeeks
+        System.out.println("GeeksForGeeks");
+    }
+}
